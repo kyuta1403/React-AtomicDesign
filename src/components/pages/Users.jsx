cf1052f2a533b10a161e0d3bf14b136821a8d4fb
kyuta1403 <96819556+kyuta1403@users.noreply.github.com>
2022-01-10 02:38:16 +0900

    Initial commit

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `nametest${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "123456@example.com",
+    phone: "090-1111-1111",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "http:wwww.goole.com"
+  };
+});
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
